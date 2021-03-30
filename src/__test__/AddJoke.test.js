df9679a530cf00de42efc256ab824ecd0719503d
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2021-03-30 09:22:06 -0700

    begin setting up tests for AddJokeModal

--- src/__test__/AddJoke.test.js
+++ src/__test__/AddJoke.test.js
@@ -2,15 +2,40 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import renderer from 'react-test-renderer'
 
-import { AddJoke } from '../components/AddJoke'
+import AddJoke from '../components/modals/AddJokeModal'
 // import 'jest-styled-components';
+
+const props = {
+  callAddJoke() {
+    console.log('callAddJoke')
+  },
+  handleClose() {
+    console.log('handleClose')
+  },
+  handleKeywordsChange() {
+    console.log('handleKeywordsChange')
+  },
+  handleValueChange() {
+    console.log('handleValueChange')
+  },
+  handleSetIsPrivate() {
+    console.log('handleSetIsPrivate')
+  },
+  newJoke: {
+    dadjokeanswer: '',
+    dadjokequestion: '',
+    isprivate: false,
+    keywords: [],
+  },
+  showAddJokeModal: false,
+}
 
 describe('AddJoke component', () => {
   it('renders correctly', () => {
     const tree = renderer
       .create(
         <Router>
-          <AddJoke addJoke={() => {}} />
+          <AddJoke {...props} />
         </Router>,
       )
       .toJSON()
